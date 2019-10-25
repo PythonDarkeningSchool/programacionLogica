6351d43a8e51b66b7e97e9f4dfff906b3cef1975
Humberto Israel Perez Rodriguez <humberto.rodriguez@itexico.com>
2019-10-25 15:20:20 -0500

    update

--- Proyecto/src/proyecto/pelotasJPanel.java
+++ Proyecto/src/proyecto/pelotasJPanel.java
@@ -43,11 +43,11 @@ public class pelotasJPanel extends javax.swing.JPanel implements Runnable {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 731, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 475, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
